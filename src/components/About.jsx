680797c3cd114cf38d0d871dc58c5895efa75f76
Rostyslav <volkov.rostyslav.8@gmail.com>
2024-02-01 12:26:55 +0200

    Update CV path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import '../styles/About.css';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import PropTypes from 'prop-types';
-import CV from '../assets/NodeJS_Dev_Rostyslav_Volkov';
+import CV from '../assets/NodeJS_Dev_Rostyslav_Volkov.pdf';
 
 const About = ({ aboutRef }) => {
   const { scrollYProgress } = useScroll({
